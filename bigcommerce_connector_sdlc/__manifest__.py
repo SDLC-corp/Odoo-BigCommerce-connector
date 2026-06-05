8cdeb43126cb934fd5db68efac7b26ac68bc1fe9
dharmrajSDLC <dharmraj@sdlccorp.com>
2026-06-05 18:30:36 +0530

    update banner

--- bigcommerce_connector_sdlc/__manifest__.py
+++ bigcommerce_connector_sdlc/__manifest__.py
@@ -90,7 +90,7 @@
             "bigcommerce_connector_sdlc/static/src/css/bigcommerce_dashboard.css",
         ],
     },
-    "images": ["static/description/banner.png"],
+    "images": ["static/description/banner.gif"],
     "installable": True,
     "application": True,
     "auto_install": False,
